c95f0d16142ed166957e6a944a40d4d1c7c89201
SarahFawson <sarahelizabethfawson@gmail.com>
2019-02-23 13:53:00 -0500

    adding sql code

---
+++ config.py
@@ -0,0 +1,16 @@
+weather_api_key = "7a8b0fc8449617d86f9112108d8ddb7d"
+nyt_api_key = "0a511b18005a4b4a8700eb80f8ea7bd3"
+gkey = "AIzaSyDmnHSKiNcGASBXiAO3WNMK03Qa9_bHpUs"
+twitter_key = "lpQ2uGm1MbDppmpPrReuo6f5y"
+census_api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+
+gwsis_dbname = "gwsis" 
+gwsis_dbuser = "root"
+gwsis_dbpwd = "IHeartDartlings"
+
+local_gwsis_dbname = "gwsis" 
+local_gwsis_dbuser = "root"
+local_gwsis_dbpwd = "92ofaAtu"
+
+local_sql_dbuser = "root"
+local_sql_dbpwd = "92ofaAtu"
